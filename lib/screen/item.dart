5ff9874aa779819136e7c77e3c4bf4406143ad34
chatchawal <6749010006@phuketvc.ac.th>
2025-08-16 15:19:40 +0700

    Step10: make model for store data

--- lib/screen/item.dart
+++ lib/screen/item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter_67_1/model/person.dart';
 
 class Item extends StatefulWidget {
   const Item({super.key});
@@ -8,23 +9,34 @@ class Item extends StatefulWidget {
 }
 
 class _ItemState extends State<Item> {
-  List date = ["สมชาย", "สมหญิง", "สมศรี", "สมปอง", "สมจิตร"];
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: date.lenght,
+      itemCount: personList.lenght,
       itemBuilder: (context, index) {
         return Container(
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
-            color: const Colors.fromARGB(255, 255, 154, 31),
+            color: const Color.fromARGB(255, 255, 154, 31),
           ), //BoxDecoration
           margin: const EdgeInsets.symmetric(vertical: 2, horizontal: 5),
           padding: EdgeInsets.all(40),
-          child: Text(
-            date[index],
-            style: TextStyle(fontSize: 20, color: Colors.white),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                personList[index].name,
+                style: const TextStyle(fontSize: 20, color: Colors.white),
+              ),
+              Text(
+                "${personList[index].age} ปี",
+                style: const TextStyle(fontSize: 20, color: Colors.white),
+              ),
+              Text(
+                personList[index].job,
+                style: const TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ],
           ),
         );
       },
