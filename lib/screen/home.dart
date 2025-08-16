708f1080d1c937b1336c61260f2396154767cf91
chatchawal <6749010006@phuketvc.ac.th>
2025-08-16 11:25:12 +0700

    Step6: Imgge widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,31 +1,22 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
- const Home({super.key});
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      // main minAxisAligment: MainAxisAligment.start,
-      // main minAxisAligment: MainAxisAligment.center,
-      // main minAxisAligment: MainAxisAligment.end,
-      // main minAxisAligment: MainAxisAligment.spaceAround,
-      // main minAxisAligment: MainAxisAligment.spaceEvenly,
-      // main minAxisAligment: MainAxisAligment.start,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAligment: CrossAxisAligment.stretch,
-    children: [
-      Container(
-          color: Colors.red,
-          child: Text("Container1"),
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: const Text(
-            "Hello",
-           style: TextStyle(
-            fontSize: 20, 
-            color: Colors.white,
-            FontWeight: FontWeight.bold,
-            letterSpacing: 3,
-          ), // TextStyle
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset("assets/image/jrbazo.jpg", width: 200, height: 200),
+        const SizedBox(height: 20),
+        Image.network(
+          "https://raw.githubusercontent.com/jrbazo/learn_flutter_67_1/main/assets/image/jrbazo.jpg",
+          width: 200,
+          height: 200,
         ),
-      ),
+      ],
+    );
+  }
+}
