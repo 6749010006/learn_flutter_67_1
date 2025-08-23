acefc1dcce62a43795125566f88cde0c80020533
chatchawal <6749010006@phuketvc.ac.th>
2025-08-23 11:07:07 +0700

    Step 13: TextFromField, Step 14 : DropdownButton

---
+++ lib/screen/addFrom.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import 'package:learn_flutter_67_1/model/person.dart';
+
+class Addfrom extends StatefulWidget {
+  const Addfrom({super.key});
+
+  @override
+  State<Addfrom> createState() => AddfromState();
+}
+
+class AddfromState extends State<Addfrom> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Add Person",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Add Person"),
+          backgroundColor: Colors.pinkAccent,
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            children: [
+              TextFormField(decoration: InputDecoration(labelText: "Name")),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Age"),
+                keyboardType: TextInputType.number,
+              ),
+
+              DropdownButtonFormField(
+                decoration: InputDecoration(labelText: "Job"),
+                items: Job.values.map((key) {
+                  return DropdownMenuItem(value: key, child: Text(key.title));
+                }).toList(),
+                onChanged: (value) {
+                  print("value = $value");
+                },
+              ),
+              SizedBox(height: 20),
+              FilledButton(
+                onPressed: () {},
+                style: FilledButton.styleFrom(
+                  backgroundColor: Colors.pinkAccent,
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                ),
+                child: Text(
+                  "Submit",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
